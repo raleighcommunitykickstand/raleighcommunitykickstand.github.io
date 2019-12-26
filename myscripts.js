70d1671ae1f6402f945d744f3c402c80fad7516b
mjdargen <mjdargen@ncsu.edu>
2019-12-26 17:01:05 -0500

    Initial commit

---
+++ myscripts.js
@@ -0,0 +1,27 @@
+/* slideshow functions */
+function plusDivs(n) {
+	showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+	var i;
+	var x = document.getElementsByClassName("mySlides");
+	if (n > x.length) {slideIndex = 1}
+	if (n < 1) {slideIndex = x.length}
+	for (i = 0; i < x.length; i++) {
+		 x[i].style.display = "none";
+	}
+	x[slideIndex-1].style.display = "block";
+}
+
+function facebookCardClick() {
+	window.open('https://www.facebook.com/RaleighCommunityKickstand/', '_blank');
+}
+
+function emailCardClick() {
+	location.href = "mailto:raleighcommunitykickstand@gmail.com";
+}
+
+function paypalCardClick() {
+	var x = document.getElementById("paypalForm").submit();
+}
