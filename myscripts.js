5064416e72b3c8cc12f46ac7306be6c24756b94a
mjdargen <mjdargen@gmail.com>
2022-05-21 16:25:29 -0400

    fixed font size and removed old scripts

--- myscripts.js
+++ myscripts.js
@@ -1,19 +1,3 @@
-/* slideshow functions */
-function plusDivs(n) {
-	showDivs(slideIndex += n);
-}
-
-function showDivs(n) {
-	var i;
-	var x = document.getElementsByClassName("mySlides");
-	if (n > x.length) {slideIndex = 1}
-	if (n < 1) {slideIndex = x.length}
-	for (i = 0; i < x.length; i++) {
-		 x[i].style.display = "none";
-	}
-	x[slideIndex-1].style.display = "block";
-}
-
 function facebookCardClick() {
 	window.open('https://www.facebook.com/RaleighCommunityKickstand/', '_blank');
 }
